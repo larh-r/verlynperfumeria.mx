aede81741af048871cfd78dcdd359103392c5ce4
larh-r <romerohermoluisantonio@gmail.com>
2026-05-10 22:53:23 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 function App() {
   return (
-    <div>
-      Verlyn Perfumería
+    <div style={{ padding: "20px" }}>
+      <h1>Verlyn Perfumería</h1>
+      <p>Bienvenido a nuestra tienda.</p>
+
+      <div>
+        <h2>Productos</h2>
+        <ul>
+          <li>Perfume 1</li>
+          <li>Perfume 2</li>
+          <li>Perfume 3</li>
+        </ul>
+      </div>
     </div>
   );
 }
